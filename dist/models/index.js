2fc3dacdacd0c4b878b4aa8120ce5704582fe038
Michael Douglas Barbosa Araujo <michaeldouglas010790@gmail.com>
2018-02-22 14:32:43 -0300

    Ajuste

--- dist/models/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const path = require("path");
-const basename = path.basename(module.filename);
-console.log(module.filename);
